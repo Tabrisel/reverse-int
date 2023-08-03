8fd9e8b315d3d221b613818d8e60cdd0c97093cd
Elizaveta Zaharova <LizaZaharova99@mail.ru>
2023-08-03 07:23:00 +0300

    feat: finish the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let a = Math.abs(n);
+    let result = String("");
+    while (a != 0) {
+        let last_char = String(a % 10);
+        a = Math.floor(a / 10);
+        result = result + last_char;
+    }
+    return result;
 }
